c5380e760ad88bf203ade88e019bd960a77be5d2
Anna V <annavakulyuk@gmail.com>
2021-02-05 16:16:26 +0100

    fetch paramsHistory with pagination

--- src/components/App.js
+++ src/components/App.js
@@ -8,30 +8,6 @@ import DepositAmountTextbox from './DepositAmountTextbox.js';
 import Graph from './Graph.js';
 import CurrentAPYDisplay from './CurrentAPYDisplay.js';
 
-const data = [
-  {
-    name: 'Page A', Principle: 4000, Interest: 2400,
-  },
-  {
-    name: 'Page B', Principle: 3000, Interest: 1398,
-  },
-  {
-    name: 'Page C', Principle: 2000, Interest: 9800,
-  },
-  {
-    name: 'Page D', Principle: 2780, Interest: 3908,
-  },
-  {
-    name: 'Page E', Principle: 1890, Interest: 4800,
-  },
-  {
-    name: 'Page F', Principle: 2390, Interest: 3800,
-  },
-  {
-    name: 'Page G', Principle: 3490, Interest: 4300,
-  },
-];
-
 function App() {
   const [asset, setAsset] = useState("DAI");
   const [deposit, setDeposit] = useState(1000);
@@ -40,12 +16,11 @@ function App() {
     <div className="App">
       <Header />
       <SelectTag onChange={setAsset} value={asset} />
-      <DepositAmountTextbox onChange={setDeposit} value={deposit}/>
-      <CurrentAPYDisplay asset={asset}/>
-      <div class='graph'>
-        <Graph data={data}/>
+      <DepositAmountTextbox onChange={setDeposit} value={deposit} />
+      <CurrentAPYDisplay asset={asset} />
+      <div class='Graph'>
+        <Graph asset={asset} />
       </div>
-      <List />
     </div>
   );
 }
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,57 +1,142 @@
-import React, { PureComponent } from 'react';
+import { invariant } from 'ts-invariant';
+import { isArray } from 'lodash';
+import React, { useEffect } from 'react';
 import { gql, useQuery } from '@apollo/client';
+import { getAverageRate } from '../helpers.js';
 import {
-    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
+    AreaChart, Area, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip,
   } from 'recharts';
 
-const GET_ASSET_DATA = gql`
-  query GetAssetData {
-    reserves (where: {symbol: $symbol}, orderBy: symbol) {
-        symbol
-        name
-        decimals
-        utilizationRate
-        averageStableRate
-        reserveFactor
-        borrowingEnabled
-        totalLiquidity
-        totalCurrentVariableDebt
-        totalPrincipalStableDebt
-    }
-}
+const GET_CURRENT_ASSET_DATA_FOR_AVG_APY = gql`
+  query ReservesRatesCurrent($symbol: String!) {
+      reserves(where: {symbol: $symbol}) {
+          id
+          pool
+          symbol
+          lastUpdateTimestamp
+          liquidityIndex
+          variableBorrowIndex
+      }
+  }  
+`;  
+
+const GET_HISTORICAL_ASSET_DATA_FOR_AVG_APY = gql`
+    query ReservesRatesHistory($timestamp_gt: Int, $timestamp_lte: Int, $symbol: String!, $first: Int) {
+        reserves(where: {symbol: $symbol}) {
+            paramsHistory(where: {timestamp_gt: $timestamp_gt, timestamp_lte: $timestamp_lte}, first: $first, orderBy: timestamp, orderDirection: asc) {
+                variableBorrowIndex
+                liquidityIndex
+                timestamp
+            }
+        }
+    }  
 `;
 
-// function Graph() {
-//     const { loading, error, data } = useQuery(GET_ASSET_DATA);
+const data_graph = [
+    {
+      day: 'Page A', Principle: 4000, Interest: 2400,
+    },
+    {
+      day: 'Page B', Principle: 3000, Interest: 1398,
+    },
+    {
+      day: 'Page C', Principle: 2000, Interest: 9800,
+    },
+    {
+      day: 'Page D', Principle: 2780, Interest: 3908,
+    },
+    {
+      day: 'Page E', Principle: 1890, Interest: 4800,
+    },
+    {
+      day: 'Page F', Principle: 2390, Interest: 3800,
+    },
+    {
+      day: 'Page G', Principle: 3490, Interest: 4300,
+    },
+];
 
-//     if (loading)
-//         return 'Loading...';
-//     if (error)
-//         return `Error! ${error.message}`;
+const Graph = ({asset}) => {
 
-// }
+    const { loading, error, data, fetchMore } = useQuery(GET_HISTORICAL_ASSET_DATA_FOR_AVG_APY, {
+        variables: {
+            symbol: asset,
+            timestamp_gt: 1609858800, //convert to a function
+            timestamp_lte: 1612537620, //convert to a function, starts at now...
+            first: 1000
+        },
+    });
 
-// const Graph = ({ data }) => (
+    console.log(data);
 
-// );
+    useEffect(() => {
+        if (!data)
+            return;
 
-const Graph = ({ data }) => {
+        let keepScanning = true;
+
+        if (keepScanning){
+            // debugger;
+            const maxTimestamp = Math.max(...data.reserves[0].paramsHistory.map(({ timestamp }) => timestamp));
+            fetchMore({
+                variables: {
+                    timestamp_gt: maxTimestamp
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) {
+                        return prev;
+                    }
+
+                    if (!fetchMoreResult.reserves[0].paramsHistory.length) {
+                        keepScanning = false;
+                        return prev;
+                    }
+
+                    invariant(isArray(prev.reserves[0].paramsHistory), "prev paramsHistory expected to be an array");
+                    invariant(isArray(fetchMoreResult.reserves[0].paramsHistory), "fetchMoreResult paramsHistory expected to be an array");
+
+                    return {
+                        reserves: [
+                            {
+                                paramsHistory: [
+                                    ...prev.reserves[0].paramsHistory,
+                                    ...fetchMoreResult.reserves[0].paramsHistory,
+                                ]
+                            }
+                        ]
+                    };
+                }
+            });
+        }
+
+        return () => {
+            keepScanning = false;
+        }
+    }, [data])
+
+    if (loading)
+        return 'Loading...';
+    if (error)
+        return `Error! ${error.message}`;  
+
+    console.log("Rendering with data.reserves[0].paramsHistory.length", data.reserves[0].paramsHistory.length);
+    
     return (
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        margin={{
-          top: 10, right: 30, left: 0, bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="Principle" stackId="1" stroke="#8884d8" fill="#B6509E" />
-        <Area type="monotone" dataKey="Interest" stackId="1" stroke="#82ca9d" fill="#2EBAC6" />
-      </AreaChart>
+        <ResponsiveContainer width="85%" height="85%">
+            <AreaChart
+                data={data_graph}
+                margin={{
+                top: 30, right: 30, left: 30, bottom: 15,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis label={{ value: 'Day', position: "insideBottom", offset: -10 }} dataKey="day" />
+                <YAxis />
+                <Tooltip />
+                <Area type="monotone" dataKey="Principle" stackId="1" stroke="#8884d8" fill="#B6509E" />
+                <Area type="monotone" dataKey="Interest" stackId="1" stroke="#82ca9d" fill="#2EBAC6" />
+            </AreaChart>
+        </ResponsiveContainer>
     );
 }
 
