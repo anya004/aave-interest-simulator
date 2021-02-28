2b7e89941284c1a547ed775db5ee04b9fe28c31b
Anna V <annavakulyuk@gmail.com>
2021-02-28 16:55:34 +0100

    use the hook for data loading

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,6 +2,7 @@ import { invariant } from 'ts-invariant';
 import { cloneDeep, isArray } from 'lodash';
 import React, { useState, useMemo, useEffect } from 'react';
 import { gql, useQuery } from '@apollo/client';
+import { useParamsHistoryData } from '../hooks/useParamsHistoryData';
 import { getAverageRate, formatGraphData , formatGraphDataVariableBorrowed} from '../helpers.js';
 import {
     AreaChart, Area, LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer, ComposedChart, Tooltip, Legend
@@ -70,215 +71,44 @@ const Graph = ({asset, deposit, borrowAsset, borrowAmount, graphType, currencySe
         fetchPolicy: 'cache-and-network',
     });
 
-    const maxTimestamp = useMemo(
-        () => typeof data !== 'undefined' 
-                ? Math.max(...data.reserves[0].paramsHistory.map(({ timestamp }) => timestamp))
-                : undefined,
-        [data]);
+    const {
+        data: dataAvg,
+        loading: loadingAvg,
+        error: errorAvg,
+      } = useParamsHistoryData(
+        GET_HISTORICAL_ASSET_DATA_FOR_AVG_APY,
+        asset,
+        now,
+        daysAgo30
+      );
     
-    // function fetchMoreData(timestamp) {
-    //     while () {
-    //         if (networkStatus != 1 || networkStatus != 3) {
-    //             continue;
-    //         }
-            
-    //         if (data.reserves[0].paramsHistory.length) {
-                
-    //         }
-            
+    const {
+        data: dataBor,
+        loading: loadingBor,
+        error: errorBor,
+      } = useParamsHistoryData(
+        GET_HISTORICAL_ASSET_DATA_FOR_VARIABLE_RATE,
+        asset,
+        now,
+        daysAgo30
+      );
 
-    //         fetchMore({
-    //             variables: {
-    //                 timestamp_gt: timestamp
-    //             },
-    //         });
-    //     }
-    // }
     
-    const fetchedData = useMemo(
-        () => {
-            let keepScanning = true;
-
-            console.log("Network status", networkStatus);
-            if (!data) {
-                console.log("Supposedly no data..", data);
-                return;
-            }
-            
-            if (loading) {
-                console.log("Stop useMemo because query is currently loading", loading);
-                //return;
-            }
-    
-            console.log("About to call fetchMore in useMemo");
-            console.log("maxTimestamp:", maxTimestamp)
-            if (networkStatus != 1 || networkStatus != 3) {
-                fetchMore({
-                    variables: {
-                        timestamp_gt: maxTimestamp
-                    },
-                });
-            }
-        },
-    [maxTimestamp, data, loading]);
-
-    //does not stop reloading and fetching more    
-    // useEffect(() => {
-    //     if (!data) {
-    //         console.log("Supposedly no data..", data);
-    //         return;
-    //     }
-
-    //     if (loading) {
-    //         console.log("Stop useEffect because query is currently loading", loading);
-    //         return;
-    //     }
-
-    //     console.log("About to call fetchMore in UseEffect");
-    //     console.log("maxTimestamp:", maxTimestamp)
-    //     fetchMore({
-    //         variables: {
-    //             timestamp_gt: maxTimestamp
-    //         },
-    //     });
-        
-    // }, [maxTimestamp, loading, data]);
-
-
-    // useEffect(() => {
-    //     if (!data)
-    //         return;
-
-    //     let keepScanning = true;
-
-    //     console.log('data', JSON.parse(JSON.stringify(data)));
-    //     if (keepScanning){
-    //         // debugger;
-    //         const maxTimestamp = Math.max(...data.reserves[0].paramsHistory.map(({ timestamp }) => timestamp));
-    //         fetchMore({
-    //             variables: {
-    //                 timestamp_gt: maxTimestamp
-    //             },
-    //             updateQuery: (prev, { fetchMoreResult }) => {
-    //                 console.log("deposit query prev", prev);
-    //                 if (!fetchMoreResult) {
-    //                     return prev;
-    //                 }
-
-    //                 if (!fetchMoreResult.reserves[0].paramsHistory.length) {
-    //                     keepScanning = false;
-    //                     return prev;
-    //                 }
-
-    //                 invariant(isArray(fetchMoreResult.reserves[0].paramsHistory), "fetchMoreResult paramsHistory expected to be an array");
-
-    //                 const retval = {
-    //                     ...prev,
-    //                     reserves: [
-    //                         {
-    //                             paramsHistory: [
-    //                                 ...(prev.reserves[0].paramsHistory || []),
-    //                                 ...fetchMoreResult.reserves[0].paramsHistory,
-    //                             ]
-    //                         }
-    //                     ]
-    //                 };
-    //                 console.log('data retval', JSON.parse(JSON.stringify(retval)));
-    //                 return retval;
-    //             }
-    //         });
-    //     }
-
-    //     return () => {
-    //         keepScanning = false;
-    //         console.log("data useEffect exunt");
-    //     }
-    // }, [data]);
-
-    // const graphData = useMemo(() => {
-    //     const paramsHistory = data?.reserves?.[0]?.paramsHistory;
-    //     if (!paramsHistory) return [];
-    //     return formatGraphData(data.reserves[0].paramsHistory, deposit);
-    // }, [data]);
-    
-    //VARIABLE BORROW
-    // const { data: dataVB, error: errorVB, loading: loadingVB, fetchMore: fetchMoreVB} = useQuery(GET_HISTORICAL_ASSET_DATA_FOR_VARIABLE_RATE, {
-    //     variables: {
-    //         symbol: borrowAsset,
-    //         timestamp_gt: daysAgo30,
-    //         timestamp_lte: now,
-    //         first: 1000
-    //     },
-        //fetchPolicy: 'no-cache',
-    // });
-
-    // useEffect(() => {
-    //     if (!dataVB)
-    //         return;
-
-    //     let keepScanningVB = true;
-
-    //     console.log('dataVB', JSON.parse(JSON.stringify(dataVB)));
-    //     console.log("keepScanningVB?", keepScanningVB);
-    //     if (keepScanningVB){
-    //         // debugger;
-    //         const maxTimestampVB = Math.max(...dataVB.reserves[0].paramsHistory.map(({ timestamp }) => timestamp));
-    //         fetchMoreVB({
-    //             variables: {
-    //                 timestamp_gt: maxTimestampVB
-    //             },
-    //             updateQuery: (prev, { fetchMoreResult }) => {
-    //                 console.log("borrow query prev", prev);
-    //                 if (!fetchMoreResult) {
-    //                     return prev;
-    //                 }
-
-    //                 if (!fetchMoreResult.reserves[0].paramsHistory.length) {
-    //                     keepScanningVB = false;
-    //                     return prev;
-    //                 }
-
-    //                 invariant(isArray(fetchMoreResult.reserves[0].paramsHistory), "fetchMoreResult paramsHistory expected to be an array");
-
-    //                 const retval = {
-    //                     ...prev,
-        //                 reserves: [
-        //                     {
-        //                         paramsHistory: [
-        //                             ...(prev.reserves[0].paramsHistory || []),
-        //                             ...fetchMoreResult.reserves[0].paramsHistory,
-        //                         ]
-        //                     }
-        //                 ]
-        //             };
-
-        //             console.log('dataVB retval', JSON.parse(JSON.stringify(retval)));
-        //             return retval;
-        //         }
-        //     });
-        // }
-
-    //     return () => {
-    //         keepScanningVB = false;
-    //         console.log("dataVB useEffect exunt");
-    //     }
-    // }, [dataVB]);
-
-    if (loading || networkStatus === 3)
+    if (loadingAvg)
         return 'Loading...';
-    // if (loadingVB)
-    //     return 'Loading...';
-    if (error)
-        return `Error! ${error.message}`;
-    // if (errorVB)
-    //     return `Error! ${errorVB.message}`;
+    if (loadingBor)
+        return 'Loading...';
+    if (errorAvg)
+        return `Error! ${errorAvg.message}`;
+    if (errorBor)
+        return `Error! ${errorBor.message}`;
 
-    console.log("Rendering with fetchedData.reserves[0].paramsHistory.length", data.reserves[0].paramsHistory.length, data.reserves[0].paramsHistory.slice(-1)[0].id);
+    console.log("Rendering with fetchedData.reserves[0].paramsHistory.length", dataAvg.reserves[0].paramsHistory.length, dataAvg.reserves[0].paramsHistory.slice(-1)[0].id);
 
     // console.log("rendering with data", JSON.parse(JSON.stringify(data)), "dataVB", JSON.parse(JSON.stringify(dataVB)));
     
-    const graphData = formatGraphData(data.reserves[0].paramsHistory, deposit);
-    // const graphDataVariableBorrow = formatGraphDataVariableBorrowed(dataVB.reserves[0].paramsHistory, borrowAmount);
+    const graphData = formatGraphData(dataAvg.reserves[0].paramsHistory, deposit);
+    const graphDataVariableBorrow = formatGraphDataVariableBorrowed(dataBor.reserves[0].paramsHistory, borrowAmount);
     //console.log("graphDataVariableBorrow:", graphDataVariableBorrow);
 
     //console.log("Graph Data:", graphData);
@@ -337,19 +167,9 @@ const Graph = ({asset, deposit, borrowAsset, borrowAmount, graphType, currencySe
                     />
                     <Legend verticalAlign="top" height={36} />
                     <Line type="monotone" data={graphData} dataKey={currencySelectedOption === "native" ?  "Interest":"InterestUsd"} name="Earned Interest" stackId="1" fill="#B6509E" stroke="#B6509E" />
-                    {/* <Line type="monotone" data={graphDataVariableBorrow} dataKey={currencySelectedOption == "native" ?  "OwedInterest":"OwedInterestUsd"} name="Owed Interest" stackId="1" stroke="#2EBAC6" fill="#2EBAC6" /> */}
+                    <Line type="monotone" data={graphDataVariableBorrow} dataKey={currencySelectedOption == "native" ?  "OwedInterest":"OwedInterestUsd"} name="Owed Interest" stackId="1" stroke="#2EBAC6" fill="#2EBAC6" />
                 </ComposedChart>
             </ResponsiveContainer>
-            <button onClick={() => {
-                console.log("Before fetchMore PreviousData:", previousData);
-                console.log("maxTime:", maxTimestamp);
-                fetchMore({
-                    variables: {
-                        timestamp_gt: maxTimestamp,
-                    },
-                });
-            }
-            }/>
             </>
         );
     }
